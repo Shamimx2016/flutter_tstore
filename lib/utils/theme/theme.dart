f0a9dd4ceecaa156ee02712867ee5724cc818297
shami <shamimbd621@gmail.com>
2023-10-25 22:08:19 +0600

    Initial commit

--- lib/utils/theme/theme.dart
+++ lib/utils/theme/theme.dart
@@ -4,6 +4,8 @@ import 'package:flutter_tstore/utils/theme/custom_themes/bottom_sheet_theme.dart
 import 'package:flutter_tstore/utils/theme/custom_themes/checkbox_theme.dart';
 import 'package:flutter_tstore/utils/theme/custom_themes/chip_theme.dart';
 import 'package:flutter_tstore/utils/theme/custom_themes/elevated_button_theme.dart';
+import 'package:flutter_tstore/utils/theme/custom_themes/outlined_button_theme.dart';
+import 'package:flutter_tstore/utils/theme/custom_themes/text_field_theme.dart';
 import 'package:flutter_tstore/utils/theme/custom_themes/text_theme.dart';
 
 class TAppTheme {
@@ -20,6 +22,8 @@ class TAppTheme {
     bottomSheetTheme: TBottomSheetTheme.lightBottomSheetTheme,
     checkboxTheme: TCheckBoxTheme.lightCheckBoxTheme,
     chipTheme: TChipTheme.lightChipTheme,
+    outlinedButtonTheme: TOutlinedButtonTheme.lightOutlinedButtonTheme,
+    inputDecorationTheme: TTextFieldTheme.lightInputDecorationTheme,
   );
   static ThemeData darkTheme = ThemeData(
     useMaterial3: true,
@@ -33,5 +37,7 @@ class TAppTheme {
     bottomSheetTheme: TBottomSheetTheme.darkBottomSheetTheme,
     checkboxTheme: TCheckBoxTheme.darkCheckBoxTheme,
     chipTheme: TChipTheme.darkChipTheme,
+    outlinedButtonTheme: TOutlinedButtonTheme.darkOutlinedButtonTheme,
+    inputDecorationTheme: TTextFieldTheme.darkInputDecorationTheme,
   );
 }
